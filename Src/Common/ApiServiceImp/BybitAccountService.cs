231164d24962b60885c7844a5dae3198f5593048
wuhewuhe <dachichiwuhe@gmail.com>
2023-10-25 17:05:15 +0200

    account endpoints

--- Src/Common/ApiServiceImp/BybitAccountService.cs
+++ Src/Common/ApiServiceImp/BybitAccountService.cs
@@ -1,5 +1,11 @@
 ﻿using bybit.net.api.Models;
 using bybit.net.api.Services;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Net;
+using System.Numerics;
+using System.Security.Principal;
 
 namespace bybit.net.api.ApiServiceImp
 {
@@ -16,6 +22,14 @@ namespace bybit.net.api.ApiServiceImp
         }
 
         private const string WALLET_BALANCE = "/v5/account/wallet-balance";
+        /// <summary>
+        /// Obtain wallet balance, query asset information of each currency, and account risk rate information. By default, currency information with assets or liabilities of 0 is not returned.
+        /// The trading of UTA inverse contracts is conducted through the CONTRACT wallet.
+        /// To get Funding wallet balance, please go to this endpoint
+        /// </summary>
+        /// <param name="accountType"></param>
+        /// <param name="coin"></param>
+        /// <returns> Account Balance </returns>
         public async Task<string?> GetAccountBalance(AccountType accountType, string? coin = null)
         {
             var query = new Dictionary<string, object> { { "accountType", accountType.Value } };
@@ -24,6 +38,232 @@ namespace bybit.net.api.ApiServiceImp
                 ("coin", coin)
             );
             var result = await this.SendSignedAsync<string>(WALLET_BALANCE, HttpMethod.Get, query: query);
+            return result;
+        }
+
+        private const string UPGRADE_UTA = "/v5/account/wallet-balance";
+        /// <summary>
+        /// Upgrade to Unified Account
+        /// </summary>
+        /// <returns> UTA Result</returns>
+        public async Task<string?> UpgradeAccount()
+        {
+            var query = new Dictionary<string, object> { };
+            var result = await this.SendSignedAsync<string>(UPGRADE_UTA, HttpMethod.Post, query: query);
+            return result;
+        }
+
+        private const string ACCOUNT_BROWSE_HISTORY = "/v5/account/borrow-history";
+        /// <summary>
+        /// Get interest records, sorted in reverse order of creation time. UTA account
+        /// </summary>
+        /// <param name="currency"></param>
+        /// <param name="startTime"></param>
+        /// <param name="endTime"></param>
+        /// <param name="limit"></param>
+        /// <param name="cursor"></param>
+        /// <returns> Borrow History</returns>
+        public async Task<string?> GetAccountBorrowHistory(string? currency = null, int? startTime = null, int? endTime = null, int? limit = null, string? cursor = null)
+        {
+            var query = new Dictionary<string, object> { };
+
+            BybitParametersUtils.AddOptionalParameters(query,
+                ("currency", currency),
+                ("startTime", startTime),
+                ("endTime", endTime),
+                ("limit", limit),
+                ("cursor", cursor)
+            );
+            var result = await this.SendSignedAsync<string>(ACCOUNT_BROWSE_HISTORY, HttpMethod.Get, query: query);
+            return result;
+        }
+
+        private const string SET_COLLATERAL_COIN = "/v5/account/set-collateral-switch";
+        /// <summary>
+        /// You can decide whether the assets in the Unified account needs to be collateral coins.
+        /// </summary>
+        /// <returns>None</returns>
+        public async Task<string?> SetAccountCollateralCoin(string? coin, CollateralSwitch? collateralSwitch = null)
+        {
+            var query = new Dictionary<string, object> { };
+            BybitParametersUtils.AddOptionalParameters(query,
+               ("coin", coin),
+               ("collateralSwitch", collateralSwitch?.Value)
+           );
+            var result = await this.SendSignedAsync<string>(SET_COLLATERAL_COIN, HttpMethod.Post, query: query);
+            return result;
+        }
+
+        private const string GET_COLLATERAL_COIN = "/v5/account/set-collateral-switch";
+        /// <summary>
+        /// Get the collateral information of the current unified margin account, including loan interest rate, loanable amount, collateral conversion rate, whether it can be mortgaged as margin, etc.
+        /// </summary>
+        /// <returns>Collateral Info</returns>
+        public async Task<string?> GetAccountCollateralCoin(string? currency = null)
+        {
+            var query = new Dictionary<string, object> { };
+            BybitParametersUtils.AddOptionalParameters(query,
+               ("currency", currency)
+           );
+            var result = await this.SendSignedAsync<string>(GET_COLLATERAL_COIN, HttpMethod.Get, query: query);
+            return result;
+        }
+
+        private const string COIN_GREEKS = "/v5/asset/coin-greeks";
+        /// <summary>
+        /// Get current account Greeks information
+        /// </summary>
+        /// <returns>Coin Greeks</returns>
+        public async Task<string?> GetAccountCoinGreeks(string? baseCoin = null)
+        {
+            var query = new Dictionary<string, object> { };
+            BybitParametersUtils.AddOptionalParameters(query,
+               ("baseCoin", baseCoin)
+           );
+            var result = await this.SendSignedAsync<string>(COIN_GREEKS, HttpMethod.Get, query: query);
+            return result;
+        }
+
+        private const string FREE_RATE = "/v5/account/fee-rate";
+        /// <summary>
+        /// Get the trading fee rate.
+        /// Covers: Spot / USDT perpetual / USDC perpetual / USDC futures / Inverse perpetual / Inverse futures / Options
+        /// </summary>
+        /// <param name="category"></param>
+        /// <param name="symbol"></param>
+        /// <param name="baseCoin"></param>
+        /// <returns>Free Rates</returns>
+        public async Task<string?> GetAccountFreeRate(Category category, string? symbol = null, string? baseCoin = null)
+        {
+            var query = new Dictionary<string, object> { { "category", category.Value } };
+
+            BybitParametersUtils.AddOptionalParameters(query,
+                ("symbol", symbol),
+                ("baseCoin", baseCoin)
+            );
+            var result = await this.SendSignedAsync<string>(FREE_RATE, HttpMethod.Get, query: query);
+            return result;
+        }
+
+        private const string ACCOUNT_INFO = "/v5/account/info";
+        /// <summary>
+        /// Query the margin mode configuration of the account.
+        /// Query the margin mode and the upgraded status of account
+        /// </summary>
+        /// <returns></returns>
+        public async Task<string?> GetAccountInfo()
+        {
+            var query = new Dictionary<string, object> { };
+
+            var result = await this.SendSignedAsync<string>(ACCOUNT_INFO, HttpMethod.Get, query: query);
+            return result;
+        }
+
+        private const string TRANSACTION_LOG = "/v5/account/transaction-log";
+        /// <summary>
+        /// Query transaction logs in Unified account.
+        /// </summary>
+        /// <param name="accountType"></param>
+        /// <param name="category"></param>
+        /// <param name="currency"></param>
+        /// <param name="baseCoin"></param>
+        /// <param name="type"></param>
+        /// <param name="startTime"></param>
+        /// <param name="endTime"></param>
+        /// <param name="limit"></param>
+        /// <param name="cursor"></param>
+        /// <returns>Transaction Data</returns>
+        public async Task<string?> GetAccountTransaction(AccountType? accountType = null, Category? category = null, string? currency = null, string? baseCoin = null, TransactionType? type = null, int? startTime = null, int? endTime = null, int? limit = null, string? cursor = null)
+        {
+            var query = new Dictionary<string, object> { };
+
+            BybitParametersUtils.AddOptionalParameters(query,
+                ("accountType", accountType?.Value),
+                ("category", category?.Value),
+                ("currency", currency),
+                ("baseCoin", baseCoin),
+                ("type", type?.Value),
+                ("startTime", startTime),
+                ("endTime", endTime),
+                ("limit", limit),
+                ("cursor", cursor)
+            );
+            var result = await this.SendSignedAsync<string>(TRANSACTION_LOG, HttpMethod.Get, query: query);
+            return result;
+        }
+
+        private const string ACCOUNT_MARGIN_MODE = "/v5/account/set-margin-mode";
+        /// <summary>
+        /// Default is regular margin mode
+        /// UTA account can be switched between these 3 kinds of margin modes, which is across UID level, working for USDT Perp, USDC Perp, USDC Futures and Options(Option does not support ISOLATED_MARGIN)
+        /// Classic account can be switched between REGULAR_MARGIN and PORTFOLIO_MARGIN, only work for USDC Perp and Options trading.
+        /// </summary>
+        /// <param name="setMarginMode"></param>
+        /// <returns>Margin Mode</returns>
+        public async Task<string?> SetAccountMarginMode(SetMarginMode? setMarginMode = null)
+        {
+            var query = new Dictionary<string, object> { };
+            BybitParametersUtils.AddOptionalParameters(query,
+               ("SetMarginMode", setMarginMode?.Value)
+           );
+            var result = await this.SendSignedAsync<string>(ACCOUNT_MARGIN_MODE, HttpMethod.Post, query: query);
+            return result;
+        }
+
+        private const string SET_MMP = "/v5/account/mmp-modify";
+        /// <summary>
+        /// Send an email to Bybit (financial.inst@bybit.com) or contact your business development (BD) manager to apply for MMP. After processed, the default settings are as below table:
+        /// </summary>
+        /// <param name="baseCoin"></param>
+        /// <param name="window"></param>
+        /// <param name="frozenPeriod"></param>
+        /// <param name="qtyLimit"></param>
+        /// <param name="deltaLimit"></param>
+        /// <returns>None</returns>
+        public async Task<string?> SetMarketMakerProtection(string? baseCoin = null, string? window = null, string? frozenPeriod = null, string? qtyLimit = null, string? deltaLimit = null)
+        {
+            var query = new Dictionary<string, object> { };
+            BybitParametersUtils.AddOptionalParameters(query,
+               ("baseCoin", baseCoin),
+               ("window", window),
+               ("frozenPeriod", frozenPeriod),
+               ("qtyLimit", qtyLimit),
+               ("deltaLimit", deltaLimit)
+           );
+            var result = await this.SendSignedAsync<string>(SET_MMP, HttpMethod.Post, query: query);
+            return result;
+        }
+
+        private const string RESET_MMP = "/v5/account/mmp-reset";
+        /// <summary>
+        /// Once the mmp triggered, you can unfreeze the account by this endpoint, then qtyLimit and deltaLimit will be reset to 0.
+        /// If the account is not frozen, reset action can also remove previous accumulation, i.e., qtyLimit and deltaLimit will be reset to 0.
+        /// </summary>
+        /// <param name="baseCoin"></param>
+        /// <returns>None</returns>
+        public async Task<string?> ResetMarketMakerProtection(string? baseCoin = null)
+        {
+            var query = new Dictionary<string, object> { };
+            BybitParametersUtils.AddOptionalParameters(query,
+               ("baseCoin", baseCoin)
+           );
+            var result = await this.SendSignedAsync<string>(RESET_MMP, HttpMethod.Post, query: query);
+            return result;
+        }
+
+        private const string MMP_STATE = "/v5/account/mmp-state";
+        /// <summary>
+        /// Get MMP State
+        /// </summary>
+        /// <param name="baseCoin"></param>
+        /// <returns>Market Maker State</returns>
+        public async Task<string?> GetMarketMakerProtectionState(string? baseCoin = null)
+        {
+            var query = new Dictionary<string, object> { };
+            BybitParametersUtils.AddOptionalParameters(query,
+               ("baseCoin", baseCoin)
+           );
+            var result = await this.SendSignedAsync<string>(MMP_STATE, HttpMethod.Get, query: query);
             return result;
         }
     }
---
+++ Src/Common/Models/TransactionType.cs
@@ -0,0 +1,40 @@
+﻿namespace bybit.net.api.Models
+{
+    public struct TransactionType
+    {
+        private TransactionType(string value)
+        {
+            Value = value;
+        }
+
+        public string Value { get; private set; }
+
+        public static TransactionType TRANSFER_IN => new("TRANSFER_IN");
+        public static TransactionType TRANSFER_OUT => new("TRANSFER_OUT");
+        public static TransactionType TRADE => new("TRADE");
+        public static TransactionType SETTLEMENT => new("SETTLEMENT");
+        public static TransactionType DELIVERY => new("DELIVERY");
+        public static TransactionType LIQUIDATION => new("LIQUIDATION");
+        public static TransactionType BONUS => new("BONUS");
+        public static TransactionType FEE_REFUND => new("FEE_REFUND");
+        public static TransactionType INTEREST => new("INTEREST");
+        public static TransactionType CURRENCY_BUY => new("CURRENCY_BUY");
+        public static TransactionType CURRENCY_SELL => new("CURRENCY_SELL");
+        public static TransactionType BORROWED_AMOUNT_INS_LOAN => new("BORROWED_AMOUNT_INS_LOAN");
+        public static TransactionType PRINCIPLE_REPAYMENT_INS_LOAN => new("PRINCIPLE_REPAYMENT_INS_LOAN");
+        public static TransactionType INTEREST_REPAYMENT_INS_LOAN => new("INTEREST_REPAYMENT_INS_LOAN");
+        public static TransactionType AUTO_SOLD_COLLATERAL_INS_LOAN => new("AUTO_SOLD_COLLATERAL_INS_LOAN");
+        public static TransactionType AUTO_BUY_LIABILITY_INS_LOAN => new("AUTO_BUY_LIABILITY_INS_LOAN");
+        public static TransactionType AUTO_PRINCIPLE_REPAYMENT_INS_LOAN => new("AUTO_PRINCIPLE_REPAYMENT_INS_LOAN");
+        public static TransactionType AUTO_INTEREST_REPAYMENT_INS_LOAN => new("AUTO_INTEREST_REPAYMENT_INS_LOAN");
+        public static TransactionType TRANSFER_IN_INS_LOAN => new("TRANSFER_IN_INS_LOAN"); // Transfer In when in the liquidation of OTC loan
+        public static TransactionType TRANSFER_OUT_INS_LOAN => new("TRANSFER_OUT_INS_LOAN"); // Transfer Out when in the liquidation of OTC loan
+        public static TransactionType SPOT_REPAYMENT_SELL => new("SPOT_REPAYMENT_SELL"); // One-click repayment currency sell
+        public static TransactionType SPOT_REPAYMENT_BUY => new("SPOT_REPAYMENT_BUY"); // One-click repayment currency buy
+
+        public override readonly string ToString() => Value;
+
+        public static implicit operator string(TransactionType transactionType) => transactionType.Value;
+    }
+
+}
