e1e433f2257d778eded143134542dd0a7bf8bad9
wuhewuhe <dachichiwuhe@gmail.com>
2023-11-02 23:27:11 +0100

    add examples

--- Examples/bybit.api.console/obj/Release/net6.0/bybit.api.console.AssemblyInfo.cs
+++ Examples/bybit.api.console/obj/Release/net6.0/bybit.api.console.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("bybit.api.console")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d41af3ad70fc5a45b276992e011f754dae0e670b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("bybit.api.console")]
 [assembly: System.Reflection.AssemblyTitleAttribute("bybit.api.console")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
