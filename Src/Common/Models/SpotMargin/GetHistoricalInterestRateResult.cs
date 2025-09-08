12f98a9a4716c05f10209c59a50b3f7eeddd4abc
kolya5544 <kolya@iktm.me>
2025-09-08 22:29:24 +0300

    Added all new endpoints for Spot Margin Trade, added a few additional response models

---
+++ Src/Common/Models/SpotMargin/GetHistoricalInterestRateResult.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bybit.net.api.Models.SpotMargin
+{
+    public class GetHistoricalInterestRateResult
+    {
+        public List<HistoricalInterestRateItem>? list { get; set; }
+    }
+
+    public class HistoricalInterestRateItem
+    {
+        public long? timestamp { get; set; }
+        public string? currency { get; set; }
+        public string? hourlyBorrowRate { get; set; }
+        public string? vipLevel { get; set; }
+    }
+
+}
---
+++ Src/Common/Models/SpotMargin/GetTieredCollateralRatioResult.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bybit.net.api.Models.SpotMargin
+{
+    public class GetTieredCollateralRatioResult
+    {
+        public List<TieredCollateralCurrency>? list { get; set; }
+    }
+
+    public class TieredCollateralCurrency
+    {
+        public string? currency { get; set; }
+        public List<CollateralRatioTier>? collateralRatioList { get; set; }
+    }
+
+    public class CollateralRatioTier
+    {
+        public string? maxQty { get; set; }          // "" means +∞
+        public string? minQty { get; set; }
+        public string? collateralRatio { get; set; }
+    }
+
+}
