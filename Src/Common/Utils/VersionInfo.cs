f2efb6727eedb3200aff70669c7d4ddf27fdd121
kolya5544 <kolya@iktm.me>
2025-10-22 22:08:09 +0300

    Version bump to 1.2.1

--- Src/Common/Utils/VersionInfo.cs
+++ Src/Common/Utils/VersionInfo.cs
@@ -2,7 +2,7 @@
 {
     public static class VersionInfo
     {
-        private static readonly string version = "1.2.0";
+        private static readonly string version = "1.2.1";
 
         public static string GetVersion
         {
